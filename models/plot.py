dc0cd667899ca9186abf2d2f16f41788787dee06
Alan Ruiz Ramirez <aruizr@hmv.com>
2023-11-28 10:57:34 -0500

    Dashboard completo, pendiente modificación eje x

--- models/plot.py
+++ models/plot.py
@@ -22,8 +22,8 @@ def linear_approx(x, start, end):
     return prediction
 
 # Lee el dataframe base
-df = pd.read_excel('C:/Users/salzate/Downloads/9. Valor Ganado/data/processed/df_wbs_pr.xlsx')
-
+df = pd.read_excel('C:/Users/aruizr/OneDrive/9. Valor Ganado/data/processed/df_wbs_pr.xlsx')
+#df = pd.read_excel('/Users/ramonalzate/Downloads/9. Valor Ganado/data/processed/df_wbs_pr.xlsx')
 app = dash.Dash(__name__)
 
 max_char_length = 9
@@ -192,26 +192,21 @@ def update_spi_gauge(end_date, wbs_value):
 def update_graph(start_date, end_date, wbs_value):
     mask = (df['Fecha'] >= start_date) & (df['Fecha'] <= end_date) & (df['WBS'] == wbs_value)
     data = df.loc[mask]
-    
+    N_end_date= data.loc[data.index[(len(data.index)-1)],'FechaFin']
+    # newdate = pd.dataframe({'WBS':[wbs_value], 'Fecha':[N_end_date]})
+    # df = pd.concat([df,newdate],ignore_index=True)
+
     # Filter the 'PV' series based on the selected 'WBS' value
     pv_data = df.loc[df['WBS'] == wbs_value]
     
     # Calculate the 'EACt' series from 'end_date' to the last date
-    eact_data = df.loc[(df['Fecha'] >= end_date) & (df['WBS'] == wbs_value)].copy()
-    eact_data['EACt'] = eact_data['AcAcum'].expanding().mean() * len(eact_data)
-    eact_data['VACt'] = eact_data['EV'].expanding().mean() * len(eact_data)
-    
-    # Adjust the 'EACt' series to end at the 'EAC' value of the selected end date
-    adjustment_factor = eact_data.loc[eact_data.index[-1], 'EAC'] / eact_data.loc[eact_data.index[-1], 'EACt']
-    eact_data['EACt'] *= adjustment_factor
-    adjustment_factor2 = eact_data.loc[eact_data.index[-1], 'LB Costo COP'] / eact_data.loc[eact_data.index[-1], 'VACt']
-    eact_data['VACt'] *= adjustment_factor2
-
+    eact_data = df.loc[(df['Fecha'] >= end_date) & (df['Fecha'] <= N_end_date) & (df['WBS'] == wbs_value)].copy()
     vectors_to_proyect = {'AcAcum':'EAC', 'EV':'LB Costo COP'}
     for key,value in vectors_to_proyect.items():
         start_value = df.loc[(df['Fecha'] == end_date) & (df['WBS'] == wbs_value), key].values[0]
         x_values = np.array(range(len(data), len(data) + len(eact_data)))
         end_value = df.loc[(df['Fecha'] == end_date) & (df['WBS'] == wbs_value), value].values[0]
+        print(x_values)
         eact_data[f'{value}t'] = linear_approx(x_values, start_value, end_value)
     
     #_______________________
